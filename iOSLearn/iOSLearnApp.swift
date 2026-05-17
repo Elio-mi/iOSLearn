b135a1668830a9f0431d4819983f6543e401c0f1
qingyang <m1448554339@gmail.com>
2026-05-17 22:10:07 +0800

    Initial Commit

---
+++ iOSLearn/iOSLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSLearnApp.swift
+//  iOSLearn
+//
+//  Created by qingyang on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
